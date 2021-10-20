0ecce5f6bacc7f0a0f9798d025d0501f9ba7fba1
Jesus Mejia <chucho4720013@gmail.com>
2021-10-20 16:31:40 -0500

    Computed properties

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <script setup>
-import {ref} from 'vue';
+import {ref, computed} from 'vue';
 
-const name = ref('');
-const url = ref('http://github.com')
+const name = ref('')
+const lastName = ref('')
+const fullname = computed(() => `${name.value} ${lastName.value}`)
+const birthday = ref(new Date())
+const current = new Date()
+console.log(birthday.value)
+const edad = computed(() => {
+  console.log(new Date(birthday.value))
+  const fecha = new Date().getTime() - new Date(birthday.value).getTime()
+  return Math.round(fecha/(1000*60*60*24*365))
+})
 </script>
 
 <template lang="pug">
 input(v-model="name")
-p {{name}}
-
-a(:href="url") Link
+input(v-model="lastName")
+input(v-model="birthday" type="date")
+p {{ fullname }}
+p edad: {{ edad }}
 </template>
 
 <style lang="sass">
