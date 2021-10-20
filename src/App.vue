7c6f7203256f3e48062be50e1ad49742a2a8d727
Jesus Mejia <chucho4720013@gmail.com>
2021-10-20 01:58:31 -0500

    Sass and bulma

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,6 @@ const msg = 'Hello World'
   <h1>{{ msg }}</h1>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+@import './scss/main.scss'
 </style>
