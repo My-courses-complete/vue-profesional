3ad1b0796e8ade4426941220d1a681e580802e3d
Jesus Mejia <chucho4720013@gmail.com>
2021-10-21 02:24:35 -0500

    API-REST get data

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,48 @@
 
 <script>
+import trackService from './services/track'
+
 export default {
   data () {
     return {
-      name: 'Jesus task',
-      tasks: [],
-      newTask: {
-        title: '',
-        time: undefined
-      }
+      searchQuery: '',
+      tracks: []
     }
   },
   computed: {
-    showTask () {
-      return this.tasks
-    },
-    totalTime () {
-      if (this.tasks.length === 0) {
-        return 0
-      }
-      let total = 0
-      this.tasks.map((t) => total += t.time) // eslint-disable-line
-      return total
+    searchMessage () {
+      return `Encontrados: ${this.tracks.length}`
     }
   },
-  created () {
-    this.tasks = JSON.parse(localStorage.getItem('tasks')) || []
-  },
   methods: {
-    addTask () {
-      if (!this.newTask.title || !this.newTask.time) {
+    search () {
+      if (!this.searchQuery) {
         return
       }
-      this.tasks.push({ title: this.newTask.title, time: this.newTask.time })
-      this.newTask.title = ''
-      this.newTask.time = 0
-      localStorage.setItem('tasks', JSON.stringify(this.tasks))
-    },
-    removeTask (i) {
-      this.tasks.splice(i, 1)
-      localStorage.setItem('tasks', JSON.stringify(this.tasks))
-    },
-    cancel () {
-      this.newTask.title = ''
-      this.newTask.time = 0
+      trackService.search(this.searchQuery)
+        .then(res => {
+          this.tracks = res.tracks.items
+        })
     }
   }
 }
-
 </script>
 
 <template lang="pug">
-.section
+section.section
+  nav.nav.has-shadow
+    .container
+      input.input.is-large(type="text", placeholder="Buscar canciones", v-model="searchQuery")
+      a.button.is-info.is-large(@click="search") Buscar
+      a.button.is-danger.is-large &times;
   .container
-    h1 {{ name }} - Total tiempo {{ totalTime }}
-  .container
-    ul(v-if="tasks.length > 0")
-      li(v-for="(t,i) in showTask")
-        h3 {{ i }}. {{t.title}} | {{t.time}} horas
-        button(@click="removeTask(i)") &times;
-    h3(v-else) You havent task
-  .container
-    h2 Create new task
-    input(type="text" placeholder="add title a new task" v-model="newTask.title")
-    input(type="number" v-model="newTask.time" placeholder="How long will it take in hours")
-    button(@click="addTask") Add
-    button(@click="cancel") Cancel
+    p
+      small {{ searchMessage }}
+
+  .container.results
+    .columns
+      .column(v-for="t in tracks")
+        | {{ t.name }}-{{ t.artists[0].name }}
 </template>
 
 <style lang="scss">
--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,9 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     open: true
+  },
+  build: {
+    sourcemap: true,
   }
 }
 )
